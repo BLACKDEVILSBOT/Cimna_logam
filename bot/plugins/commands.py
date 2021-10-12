2c1c5b1197853537cd04c032dfd961c33d4746e7
BLACK_DEVIL <91047016+BLACKDEVILSBOT@users.noreply.github.com>
2021-10-12 10:24:22 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'GROUP', url="https://t.me/CinemaLokhm"
                                 )
                         ]
                     ]
